226d10af6eca29ac443efb5a703ea6450827f678
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2021-12-15 07:53:30 +0100

    compiler

--- compile.js
+++ compile.js
@@ -1,1 +1,8 @@
-// compile code will go here
+const path = require('path'); //creates a path to the .sol file
+const fs = require('fs'); //filesystem module
+const solc = require('solc'); //solidity compiler
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); //generates a path to inbox.sol
+const source  = fs.readFileSync(inboxPath, 'utf8') //reads th content of the file, state encoding
+
+solc.compile(source, 1); //also define the number of contracts
