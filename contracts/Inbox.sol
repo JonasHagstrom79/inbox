6a1adca469260bfbca3657fdf04b75b076fe5309
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2021-12-15 07:19:29 +0100

    contract.sol

---
+++ contracts/Inbox.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.17;
+// linter warnings (red underline) about pragma version can igonored!
+
+// contract code will go here
+contract Inbox{
+    string public message;
+
+    function Inbox(string initialMessege) public {
+        message = initialMessage;
+    }
+
+    function setMesage(string newMessage) public {
+        message = newMessage
+    }
+}
