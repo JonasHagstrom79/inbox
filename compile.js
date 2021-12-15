ef823c7aca5d276ac38bcaa0ded5ac84883c987e
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2021-12-15 08:03:35 +0100

    added comments

--- compile.js
+++ compile.js
@@ -3,6 +3,6 @@ const fs = require('fs'); //filesystem module
 const solc = require('solc'); //solidity compiler
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); //generates a path to inbox.sol
-const source  = fs.readFileSync(inboxPath, 'utf8') //reads th content of the file, state encoding
+const source  = fs.readFileSync(inboxPath, 'utf8'); //reads th content of the file, state encoding
 
-solc.compile(source, 1); //also define the number of contracts
+console.log(solc.compile(source, 1)); //also define the number of contracts
--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,15 +1,13 @@
 pragma solidity ^0.4.17;
-// linter warnings (red underline) about pragma version can igonored!
 
-// contract code will go here
 contract Inbox{
     string public message;
 
-    function Inbox(string initialMessege) public {
+    function inbox(string initialMessage) public {
         message = initialMessage;
     }
 
-    function setMesage(string newMessage) public {
-        message = newMessage
-    }
+    function setMessage(string newMessage) public{
+        message = newMessage;
+    }     
 }
