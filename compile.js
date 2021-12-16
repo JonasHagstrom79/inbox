45eb3cd4ec9d4bca5c6ba34dabdbd12144ee42a0
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2021-12-16 07:39:35 +0100

    Testing with Mocha

--- compile.js
+++ compile.js
@@ -5,4 +5,5 @@ const solc = require('solc'); //solidity compiler
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); //generates a path to inbox.sol
 const source  = fs.readFileSync(inboxPath, 'utf8'); //reads th content of the file, state encoding
 
-console.log(solc.compile(source, 1)); //also define the number of contracts
+module.exports = solc.compile(source, 1).contracts[':Inbox']; //allows exporting the object, 1defines the number of contracts, [':']object bytecode property(contracts source)
+//console.log(solc.compile(source, 1));
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,34 @@
-// contract test code will go here
+const assert = require('assert'); //
+const ganache = require('ganache-cli'); //local ethereum test network
+const Web3 = require('web3'); //a constructor function
+//local instance of web3
+const web3 = new Web3(ganache.provider());
+
+//Test with Mocha
+class Car{
+    park(){
+        return 'stopped';
+    }
+
+    drive(){
+        return 'vroom';
+    }
+}
+//Mocha
+let car; //To make it accessible to the it-statements
+
+beforeEach(()=> {
+    car =  new Car();
+});
+describe('Car', () => { //Arrowfunction with all test statements(it) for class 'Car'
+    
+    it('can park', () =>{ //test setup and assertion logic inside the arrow function
+        //const car =  new Car();//Double call, use beforeEach to not repeat yourself
+        assert.equal(car.park(), 'stopped'); //If the test is equal it fill pass
+    }); 
+
+    it('can drive', () =>{
+        //const car =  new Car();
+        assert.equal(car.drive(), 'vroom');
+    });
+});
