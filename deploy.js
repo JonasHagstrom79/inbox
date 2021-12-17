5b4fd5e798bf4cd3dc8e2b33a8822df1ace8d5f1
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2021-12-17 09:19:57 +0100

    deploy.js

--- deploy.js
+++ deploy.js
@@ -1,1 +1,12 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider'); //our wallet
+const Web3 = require('web3'); //web3 constructor
+const { interface, bytecode } = require('./compile'); //from our compile script
+
+
+//setup for HDWalletprovider
+const provider = new HDWalletProvider(
+    'tennis make claw mad mosquito gorilla slab wide govern angry bomb turtle', //acount mnominic
+    'https://rinkeby.infura.io/v3/f4a45206e8ba4badada0e2f8197ce831' //link to the testnetwork
+);
+//connection to the network
+const web3 = new Web3(provider);
